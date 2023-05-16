d61e405dc86c77e0eadb2717c44aa297c0fbc8b0
Piele Matei <99535423+mpiele@users.noreply.github.com>
2023-05-16 22:53:27 +0300

    Update example.py

--- example.py
+++ example.py
@@ -1,10 +1,10 @@
 #EXAMPLE OF USE
 
-#import the crypt and decrypt functions
-from bobcrypt import Crypt_ABC, Decrypt_ABC
+#import the encrypt and decrypt functions
+from bobcrypt import Encrypt_ABC, Decrypt_ABC
 
-#crypts the string "hello, world!", should output "8w5w12w12w15w,wxw23w15w18w12w4w!w"
-print(Crypt_ABC("hello, world!"))
+#encrypts the string "hello, world!", should output "8w5w12w12w15w,wxw23w15w18w12w4w!w"
+print(Encrypt_ABC("hello, world!"))
 
 #decrypts the string "8w5w12w12w15w,wxw23w15w18w12w4w!w", should output "hello, world!"
 print(Decrypt_ABC("8w5w12w12w15w,wxw23w15w18w12w4w!w"))
